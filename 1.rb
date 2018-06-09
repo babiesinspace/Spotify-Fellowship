57a5130fb9da2a0bacbe1b292e96520c6f87848c
Alexandra Cooper <allie.g.cooper@gmail.com>
2018-06-09 12:28:16 -0400

    commented out all but solution

--- 1.rb
+++ 1.rb
@@ -2,20 +2,20 @@ require 'benchmark'
 
 # Question 1 -- sortByStrings(s,t): Sort the letters in the string s by the order they occur in the string t. You can assume t will not have repetitive characters. For s = "weather" and t = "therapyw", the output should be sortByString(s, t) = "theeraw". For s = "good" and t = "odg", the output should be sortByString(s, t) = "oodg".
 
-def sortByStrings1(s, t)
-  str_to_sort = s.split("")
-  sort_by_str = t.split("")
-  answer = ""
+# def sortByStrings1(s, t)
+#   str_to_sort = s.split("")
+#   sort_by_str = t.split("")
+#   answer = ""
 
-  sort_by_str.each do |letter|
-    str_to_sort.each do |character|
-      if letter == character
-        answer += letter
-      end 
-    end
-  end
-  answer
-end 
+#   sort_by_str.each do |letter|
+#     str_to_sort.each do |character|
+#       if letter == character
+#         answer += letter
+#       end 
+#     end
+#   end
+#   answer
+# end 
 
 
 def sortByStrings2(s, t)
@@ -43,19 +43,19 @@ def sortByStrings2(s, t)
 end 
 
 
-Benchmark.bm do |benchmark|
-  benchmark.report("2 loop") do
-	  50_000.times do
-	    sortByStrings1("weather","therapywgolzqp")
-	  end
-  end
+# Benchmark.bm do |benchmark|
+#   benchmark.report("2 loop") do
+# 	  50_000.times do
+# 	    sortByStrings1("weather","therapywgolzqp")
+# 	  end
+#   end
 
-  benchmark.report("hash") do
-    50_000.times do
-    	sortByStrings2("weather","therapywgolzqp")
-  	end
-  end
-end
+#   benchmark.report("hash") do
+#     50_000.times do
+#     	sortByStrings2("weather","therapywgolzqp")
+#   	end
+#   end
+# end
 
 
 #sortByStrings("good","odg")
