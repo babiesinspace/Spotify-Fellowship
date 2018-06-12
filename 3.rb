f64fe88d7c50d4e7f4fa294a4aae1dbba0d49169
Alexandra Cooper <allie.g.cooper@gmail.com>
2018-06-12 16:30:00 -0400

    solution using recursion but it counts repeats..

--- 3.rb
+++ 3.rb
@@ -1,8 +1,12 @@
-def count(denominations, num_of_coins, amount) 
+def count(denominations, amount) 
   #add to count if change is made
-  return 1 if amount === 0
-  #if there are no coin denominations left to check but we still have change, or if the amount given is 0 or negative 
-  return 0 if (num_of_coins < 0 && amount > 0) || amount <= 0
+  return 1 if amount == 0
+  #if there are no coin denominations left to check but we still have change, or if the amount is negative 
+  return 0 if amount < 0
 
-
+  ways_to_make_change = 0
+  denominations.each do |coin|
+  	ways_to_make_change += count(denominations, (amount - coin))
+  end 
+  ways_to_make_change
 end
