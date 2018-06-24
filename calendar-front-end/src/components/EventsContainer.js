be1ab6843544cd62cfb3ac02f0bd3f0db7be172b
babiesinspace <allie.g.cooper@gmail.com>
2018-06-24 14:14:06 -0400

    add moment.js for date manipulation

---
+++ calendar-front-end/src/components/EventsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EventsContainer extends Component {
+  render() {
+    return (
+      <div>
+        Events
+      </div>
+    )
+  }
+}
+
+export default EventsContainer
