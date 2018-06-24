e28c0a890e12322fe6ef30164d601621e7a2b1eb
babiesinspace <allie.g.cooper@gmail.com>
2018-06-24 13:20:38 -0400

    seed some events, save the validations on app model, remember to reenable auto-save..

--- CalendarBackend/db/seeds.rb
+++ CalendarBackend/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+events = Event.create([
+	{title: "Physical Therapy", start: Time.new(2018, 6, 24, 14, 0, 0, "+04:00"), end: Time.new(2018, 6, 24, 15, 0, 0, "+04:00"), body: "PT"},
+	{title: "Vacay", start: Time.new(2018, 6, 30, 9, 0, 0, "+04:00"), end: Time.new(2018, 7, 5, 15, 0, 0, "+04:00"), body: "Woodstock baby"},
+	{title: "Interview", start: Time.new(2018, 6, 18, 11, 0, 0, "+04:00"), end: Time.new(2018, 6, 24, 11, 30, 0, "+04:00"), body: "Phone"},
+	{title: "Interview", start: Time.new(2018, 6, 26, 10, 0, 0, "+04:00"), end: Time.new(2018, 6, 26, 12, 0, 0, "+04:00"), body: "Whiteboard"},
+	{title: "Plumber", start: Time.new(2018, 6, 30, 10, 0, 0, "+04:00"), end: Time.new(2018, 6, 30, 12, 0, 0, "+04:00"), body: "Confirm with Brian"},
+	{title: "Spotify Challenge", start: Time.new(2018, 6, 24, 13, 0, 0, "+04:00"), end: Time.new(2018, 6, 25, 23, 0, 0, "+04:00"), body: "Fingers crossed!"},
+])
