c7f9f3ae1ace367f83d1331fb2416893360dec16
babiesinspace <allie.g.cooper@gmail.com>
2018-06-27 13:27:19 -0400

    add axios to front end. change database back to holding datetimes instead of times due to day/mo/year error. import axios in calendar front end. add configuration to enable CORS in backend. allow all headers/methods for crud functionality in backend for front end

--- CalendarBackend/db/migrate/20180624165200_create_events.rb
+++ CalendarBackend/db/migrate/20180624165200_create_events.rb
@@ -3,8 +3,8 @@ class CreateEvents < ActiveRecord::Migration[5.2]
     create_table :events do |t|
       t.string :title, null: false
       t.text :body
-      t.time :start, null: false
-      t.time :end, null:false
+      t.datetime :start, null: false
+      t.datetime :end, null:false
 
       t.timestamps
     end
